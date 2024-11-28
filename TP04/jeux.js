dcc6b44ca395c16f5a03ffe0b038a6273900aa4a
DxSper <142800938+DxSper@users.noreply.github.com>
2024-11-28 14:22:21 +0100

    Jeu finit

--- TP04/jeux.js
+++ TP04/jeux.js
@@ -37,43 +37,72 @@ tab2.forEach(index => {
     container.appendChild(cloneTemplate); 
 });
 
+
+// Initialisation du timer
+let timerStarted = false; 
+let startTime = 0; 
+let intervalId = null; 
+// Nombre total d'images au départ
+let totalImages = tab2.length;
+// Fonction pour démarrer le compteur
+function startTimer() {
+    if (!timerStarted) {
+        timerStarted = true;
+        startTime = performance.now();
+
+        intervalId = setInterval(() => {
+            const currentTime = performance.now();
+            const elapsedTime = (currentTime - startTime) / 1000; // Temps écoulé en secondes
+            if (totalImages === 0) {
+                clearInterval(intervalId);
+                alert(`Temps écoulé : ${elapsedTime.toFixed(3)} secondes`)
+                timerStarted = false;
+                return
+            }
+        }, 10);
+    }
+}
+
 // Effacer les images lorsque je clique sur les deux identiques
 // Fonction pour gérer le clic sur une image
 let clickedImg = []
 let idClickedImg = []
+let ImgparentElement = []
 const handleImageClick = (id, imgElement) => {
-
+    // Inialiser le timer au premier clique
+    if (!timerStarted) startTimer();
+    // Récupération de l'id de l'image cliquée
     const id_img = parseInt(id, 10)
-
-
-
-
-    console.log(id)
-    console.log(id_img)
-    console.log(imgElement)
-    console.log(clickedImg)
-    console.log(idClickedImg)
-    // Si ce n'est pas le cas alors, l'ajouter dans le tableau des images cliqué
+    // l'ajouter dans le tableau des images cliqué
     clickedImg.push(imgElement);
     idClickedImg.push(id_img);
     
-
+    // Afficher un contour en vert sur l'image cliqué
+    Imgparent = imgElement.parentElement;
+    Imgparent.classList.add('green')
+    ImgparentElement.push(Imgparent)
     // Vérifier si il y a 2 éléments dans le tableau :
     if (clickedImg.length === 2) {
+        // Si deux images sont cliquées, enlever les contours verts des deux parents
+        ImgparentElement.forEach(parent => parent.classList.remove('green'));
         // Vérifier si l'image est identique via l'alt
         if (clickedImg[0].alt === clickedImg[1].alt) {
             // Vérifier que l'id n'est pas identique
             if (idClickedImg[0] !== idClickedImg[1]) {
-                // Si c'est le cas alors effacer les deux images
-                clickedImg[0].parentElement.remove();
-                clickedImg[1].parentElement.remove();
+                // Si c'est le cas alors effacer les deux images sur le rendu
+                clickedImg[0].remove();
+                clickedImg[1].remove();
+                // Informer le timer du nombre d'images restantes
+                totalImages -= 2;
                 // Vider les tableaux
                 clickedImg = []
                 idClickedImg = []
+                ImgparentElement = []
             } else {
                 // Vider les tableaux 
                 clickedImg = []
                 idClickedImg = []
+                ImgparentElement = []
             }
         } else {
             // Les images sont différentes, le tableau clickedImg est reset 
@@ -85,3 +114,4 @@ const handleImageClick = (id, imgElement) => {
         return
     }
 }
+
